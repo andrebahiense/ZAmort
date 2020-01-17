e5ef57489abbc775d70363ca33eab3e16a087729
Andre Bahiense <andregbahiense@gmail.com>
2020-01-17 03:49:46 -0300

    New version

--- zamort.ino
+++ zamort.ino
@@ -1,12 +1,23 @@
+//----------------------------------------------------------------------------------------------------------
+// ZAmort - AUSS & AUSS Game Controller
+// by Andre Bahiense. January 2020.
+//
+// This is the code for the ZAmort game controller, designed for the AUSS & AUSS transmedia experience,
+// created by NIKIMA.
+// Please visit http://www.aussnauss.com/ for more information.
+//----------------------------------------------------------------------------------------------------------
+
 #include <Keyboard.h>
 
 int bot[] = {2, 3, 4, 5};
 char key[] = {"asdf"};
-int x;
-int a;
+int stateArray[] = {0, 0, 0, 0};
+bool buttonState = true;
+bool lastButtonState = false;
+int x, y;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(300);
   Keyboard.begin();
   for (int i = 0; i < 4; i++) {
     pinMode(bot[i], INPUT);
@@ -15,16 +26,29 @@ void setup() {
 
 void loop() {
 
-  for (x = 0; x < 4; x++) {//
-    a = digitalRead(bot[x]);
-    if (a == HIGH) {
-      Keyboard.press(key[x]);
-      //Serial.println(key[x]);
-      delay(110);
-    }
-    else {
-      Keyboard.releaseAll();
-      //Serial.println("No");
+  for (x = 0; x < 4; x++) {
+    stateArray[x] = digitalRead(bot[x]);
+    delay(7);
+  }
+
+  if (stateArray[0] == HIGH || stateArray[1] == HIGH || stateArray[2] == HIGH || stateArray[3] == HIGH) {
+    buttonState = true;
+    if (buttonState != lastButtonState) {
+      for (y = 0; y < 4; y++) {
+        if (stateArray[y] == HIGH) {
+          Keyboard.press(key[y]);
+          Keyboard.releaseAll();
+        }
+        else {
+          Keyboard.releaseAll();
+        }
+      }
     }
   }
+
+  else {
+    Keyboard.releaseAll();
+    buttonState = false;
+  }
+  lastButtonState = buttonState;
 }
